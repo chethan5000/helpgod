f5228ade796e4ec90e208440657f9806b23129cc
VIVEK <VIVEK@VIVEK-PC>
2018-01-16 22:46:57 +0530

    firstjavaclass

---
+++ test/src/test/Hello.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		
+		int i =0;
+		
+		while (i<=5)
+		{
+			System.out.println("hello world"   + i);
+		}
+	}
+
+}
